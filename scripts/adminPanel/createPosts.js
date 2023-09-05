840103a78caec206eb1016b1107753b09e530305
Jacob Ortiz <98186544+theunitysharp@users.noreply.github.com>
2023-09-05 17:16:35 -0500

    Add files via upload

--- scripts/adminPanel/createPosts.js
+++ scripts/adminPanel/createPosts.js
@@ -9,9 +9,6 @@ const yyyy = today.getFullYear();
 let mm = today.getMonth() + 1; // Months start at 0!
 let dd = today.getDate();
 
-if (dd < 10) dd = '0' + dd;
-if (mm < 10) mm = '0' + mm;
-
 const datePosted = mm + '/' + dd + '/' + yyyy;
 
 datePostedContainer.value = "<span>Posted: " + datePosted + "</span>";

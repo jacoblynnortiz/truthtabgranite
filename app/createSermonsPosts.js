67fedb88d3114260a17a3508973e500f1cee6e16
Jacob Ortiz <98186544+theunitysharp@users.noreply.github.com>
2023-09-23 00:29:31 -0500

    Add files via upload

--- app/createSermonsPosts.js
+++ app/createSermonsPosts.js
@@ -1,9 +1,12 @@
 
 const sermonsContainer = document.getElementById("sermonsContainer");
 
+let viewAllSermonsBtn = document.getElementById("viewAllSermonsBtn");
+
 // FETCHES JSON FROM DATABASE 
 
 $.getJSON('https://sheetdb.io/api/v1/yn7mbvhpvnpn8', function(sermon_posts) {
+    // looks through sermons database displaying each one
     for (let i = sermon_posts.length - 1; i >= sermon_posts.length - 10; i--) {
         // creates new sermon post
         let newSermonContainer = document.createElement("div");;
@@ -11,13 +14,16 @@ $.getJSON('https://sheetdb.io/api/v1/yn7mbvhpvnpn8', function(sermon_posts) {
         let newSermonTitle = document.createElement("h2");
         let newSermonDate = document.createElement("h3");
 
+        // creates sermon video
         newSermonContainer.classList.add("sermon-container");
         newSermonContainer.setAttribute("id", i);
         newSermonContainer.innerHTML = sermon_posts[i].sermon_src;
 
+        // creates sermon title
         newSermonTitle.setAttribute("id", i);
         newSermonTitle.innerText = sermon_posts[i].sermon_title;
 
+        // creates sermon date
         newSermonDate.setAttribute("id", i);
         newSermonDate.innerText = sermon_posts[i].sermon_date;
     
@@ -27,4 +33,37 @@ $.getJSON('https://sheetdb.io/api/v1/yn7mbvhpvnpn8', function(sermon_posts) {
         newSermonInfoContainer.appendChild(newSermonTitle);
         newSermonInfoContainer.appendChild(newSermonDate);
     }
+});
+
+viewAllSermonsBtn.addEventListener('click', function() {
+    $.getJSON('https://sheetdb.io/api/v1/yn7mbvhpvnpn8', function(sermon_posts) {
+    // looks through sermons database displaying each one
+    for (let i = sermon_posts.length - 11; i >= 0; i--) {
+        // creates new sermon post
+        let newSermonContainer = document.createElement("div");;
+        let newSermonInfoContainer = document.createElement("div");;
+        let newSermonTitle = document.createElement("h2");
+        let newSermonDate = document.createElement("h3");
+
+        // creates sermon video
+        newSermonContainer.classList.add("sermon-container");
+        newSermonContainer.setAttribute("id", i);
+        newSermonContainer.innerHTML = sermon_posts[i].sermon_src;
+
+        // creates sermon title
+        newSermonTitle.setAttribute("id", i);
+        newSermonTitle.innerText = sermon_posts[i].sermon_title;
+
+        // creates sermon date
+        newSermonDate.setAttribute("id", i);
+        newSermonDate.innerText = sermon_posts[i].sermon_date;
+    
+        // adds post elements to sermons container
+        sermonsContainer.appendChild(newSermonContainer);
+        newSermonContainer.appendChild(newSermonInfoContainer);
+        newSermonInfoContainer.appendChild(newSermonTitle);
+        newSermonInfoContainer.appendChild(newSermonDate);
+    }
+    viewAllSermonsBtn.style.display = 'none';
+});
 });

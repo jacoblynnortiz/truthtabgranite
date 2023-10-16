f14a83b50e562a9656dbe7055beb8e51967c09da
Jacob Ortiz <98186544+jacoblynnortiz@users.noreply.github.com>
2023-10-16 00:31:53 -0500

    Add files via upload

--- app/sideCourseOverview.js
+++ app/sideCourseOverview.js
@@ -1,78 +1,45 @@
 let sideCourseOverview = document.getElementById('sideCourseOverview');
-let sideCourseOverviewBtn = document.getElementById('sideCourseOverviewBtn');
-
 let bibleSeriesContainer = document.getElementById('bibleSeriesContainer');
 
 let sideCourseOverviewOpen = true;
 
 if (window.matchMedia('screen and (max-width: 1200px)').matches) {
     if (sideCourseOverviewOpen == true) {
-        sideCourseOverview.style.right = '-100%';
-        sideCourseOverviewBtn.style.right = '0px';
+        sideCourseOverview.style.right = '-400px';
         sideCourseOverviewOpen = false;
-        if (window.matchMedia('screen and (max-width: 435px)').matches) {
-            sideCourseOverviewBtn.style.left = null;
-            sideCourseOverviewBtn.style.right = '0px';
-        } else {
-            sideCourseOverviewBtn.style.right = '0px';
-        }
     } else {
         sideCourseOverview.style.right = '0px';
-        if (window.matchMedia('screen and (max-width: 435px)').matches) {
-            sideCourseOverviewBtn.style.right = null;
-                sideCourseOverviewBtn.style.left = '0px';
-        } else {
-            sideCourseOverviewBtn.style.right = '400px';
-        }
         sideCourseOverviewOpen = true;
     }
 } else {
     sideCourseOverviewOpen = false;
     if (sideCourseOverviewOpen == true) {
-        sideCourseOverview.style.right = '-100%';
-        sideCourseOverviewBtn.style.right = '0px';
+        sideCourseOverview.style.right = '-400px';
         sideCourseOverviewOpen = false;
         bibleSeriesContainer.style.paddingRight = '0px';
     } else {
         sideCourseOverview.style.right = '0px';
-        sideCourseOverviewBtn.style.right = '400px';
         sideCourseOverviewOpen = true;
-        bibleSeriesContainer.style.paddingRight = '425px';
+        bibleSeriesContainer.style.paddingRight = '400px';
     }
 }
 
 function sideCourseOverviewToggle() {
     if (window.matchMedia('screen and (max-width: 1200px)').matches) {
         if (sideCourseOverviewOpen == true) {
-            sideCourseOverview.style.right = '-100%';
-            sideCourseOverviewBtn.style.right = '0px';
+            sideCourseOverview.style.right = '-400px';
             sideCourseOverviewOpen = false;
-            if (window.matchMedia('screen and (max-width: 435px)').matches) {
-                sideCourseOverviewBtn.style.left = null;
-                sideCourseOverviewBtn.style.right = '0px';
-            } else {
-                sideCourseOverviewBtn.style.right = '0px';
-            }
         } else {
             sideCourseOverview.style.right = '0px';
-            if (window.matchMedia('screen and (max-width: 435px)').matches) {
-                sideCourseOverviewBtn.style.right = null;
-                sideCourseOverviewBtn.style.left = '0px';
-            } else {
-                sideCourseOverviewBtn.style.right = '400px';
-            }
-            sideCourseOverviewBtn.style.right = '400px';
             sideCourseOverviewOpen = true;
         }
     } else {
         if (sideCourseOverviewOpen == true) {
-            sideCourseOverview.style.right = '-100%';
-            sideCourseOverviewBtn.style.right = '0px';
+            sideCourseOverview.style.right = '-400px';
             sideCourseOverviewOpen = false;
             bibleSeriesContainer.style.paddingRight = '0px';
         } else {
             sideCourseOverview.style.right = '0px';
-            sideCourseOverviewBtn.style.right = '400px';
             sideCourseOverviewOpen = true;
             bibleSeriesContainer.style.paddingRight = '425px';
         }

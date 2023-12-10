ad1ce2b61e22b2318c4a3f4f525bdd26907d7b63
Jacob Ortiz <98186544+jacoblynnortiz@users.noreply.github.com>
2023-12-10 17:25:07 -0600

    Add files via upload

--- app/adminPanel/userLogs.js
+++ app/adminPanel/userLogs.js
@@ -1,49 +1,49 @@
 $.getJSON('https://sheetdb.io/api/v1/8yk5oje5mdf3w', function (user_enrollments) {
-    let usersEnrolledDB = document.getElementById("usersEnrolledDB");
+    let userLogsTable = document.getElementById("userLogsTable");
 
     // creates charts headers
 
-    let fullName = document.createElement("th");
+    let name = document.createElement("th");
     let email = document.createElement("th");
     let message = document.createElement("th");
     let timestamp = document.createElement("th");
 
-    fullName.innerText = "Full Name";
+    name.innerText = "Full Name";
     email.innerText = "E-mail";
     message.innerText = "Message";
     timestamp.innerText = "Timestamp";
 
-    usersEnrolledDB.appendChild(fullName);
-    usersEnrolledDB.appendChild(email);
-    usersEnrolledDB.appendChild(message);
-    usersEnrolledDB.appendChild(timestamp);
+    userLogsTable.appendChild(name);
+    userLogsTable.appendChild(email);
+    userLogsTable.appendChild(message);
+    userLogsTable.appendChild(timestamp);
 
     // loops through entire database to display all logs
 
     for (let i = 0; i <= user_enrollments.length; i++) {
         // creates a new log
 
-        let fullNameV = document.createElement("td");
-        let emailV = document.createElement("td");
-        let messageV = document.createElement("td");
-        let timestampV = document.createElement("td");
+        let nameItem = document.createElement("td");
+        let emailItem = document.createElement("td");
+        let messageItem = document.createElement("td");
+        let timestampItem = document.createElement("td");
 
-        fullNameV.setAttribute("data-cell", "Full Name");
-        emailV.setAttribute("data-cell", "E-mail");
-        messageV.setAttribute("data-cell", "Message");
-        timestampV.setAttribute("data-cell", "Timestamp");
+        nameItem.setAttribute("data-cell", "Full Name");
+        emailItem.setAttribute("data-cell", "E-mail");
+        messageItem.setAttribute("data-cell", "Message");
+        timestampItem.setAttribute("data-cell", "Timestamp");
 
-        let tableRow = document.createElement("tr");
+        let userLog = document.createElement("tr");
 
-        fullNameV.innerText = user_enrollments[i].fullName;
-        emailV.innerText = user_enrollments[i].email;
-        messageV.innerText = user_enrollments[i].message;
-        timestampV.innerText = user_enrollments[i].timestamp;
+        nameItem.innerText = user_enrollments[i].fullName;
+        emailItem.innerText = user_enrollments[i].email;
+        messageItem.innerText = user_enrollments[i].message;
+        timestampItem.innerText = user_enrollments[i].timestamp;
 
-        tableRow.appendChild(fullNameV);
-        tableRow.appendChild(emailV);
-        tableRow.appendChild(messageV);
-        tableRow.appendChild(timestampV);
-        usersEnrolledDB.appendChild(tableRow);
+        userLog.appendChild(nameItem);
+        userLog.appendChild(emailItem);
+        userLog.appendChild(messageItem);
+        userLog.appendChild(timestampItem);
+        userLogsTable.appendChild(userLog);
     }
 });
